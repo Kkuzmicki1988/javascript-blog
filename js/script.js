1c753bc16afbe78de36641364baef06028951904
Kamil Kuzmicki <kkuzmicki1988@gmail.com>
2020-06-26 09:54:07 +0200

    code improvement

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 'use strict';
-/*document.getElementById('test-button').addEventListener('click', function(){
-  const links = document.querySelectorAll('.titles a');
-  console.log('links:', links);
-}); */
-
-/* Pytania do mentora na Piątek 03.07.2020
+  /* Pytania do mentora na Piątek 03.07.2020
   - W inspektorze nadawanie i usuwanie klas przy kliknieciu nie dziala jak nalezy.
   - Problem z git. Przy probie zapisania commita otrzymuje komunikat warning "There are too many unreachable loose objects; run 'git prune' to remove them."
     Should I try again? (y/n) wybor Yes powtarza pytanie wybor No zapisuje commit i wszystko dziala poprawnie. O co c'mon :) ?
-*/
+  */
 
-const titleClickHandler = function(event){
+const titleClickHandler = function(event) {
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -21,9 +15,9 @@ const titleClickHandler = function(event){
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-}
-  /* [DONE] add class 'active' to the clicked link */
+  }
 
+  /* [DONE] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active');
 
@@ -33,21 +27,19 @@ const titleClickHandler = function(event){
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-}
+  }
+
   /* [DONE] get 'href' attribute from the clicked link */
 
   const articleSelector = clickedElement.getAttribute('href');
-  console.log(articleSelector);
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
   const targetArticle = document.querySelector(articleSelector);
-  console.log(targetArticle);
 
   /* [DONE] add class 'active' to the correct article */
 
   targetArticle.classList.add('active');
-
 }
 
 const links = document.querySelectorAll('.titles a');
