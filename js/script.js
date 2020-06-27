0afb68e82386ba938ba56794fc20c33acb5c6436
Kamil Kuzmicki <kkuzmicki1988@gmail.com>
2020-06-27 15:16:57 +0200

    check

--- js/script.js
+++ js/script.js
@@ -1,49 +1,94 @@
+{
 'use strict';
   /* Pytania do mentora na Piątek 03.07.2020
   - W inspektorze nadawanie i usuwanie klas przy kliknieciu nie dziala jak nalezy.
   - Problem z git. Przy probie zapisania commita otrzymuje komunikat warning "There are too many unreachable loose objects; run 'git prune' to remove them."
     Should I try again? (y/n) wybor Yes powtarza pytanie wybor No zapisuje commit i wszystko dziala poprawnie. O co c'mon :) ?
+  - W module jest mowa aby zamykac kod w nawiasach klamrowych. Czy nawiasy klamrowe wstawiac od 1 lini kodu a konczyc na ostatniej ? chodzi przykladowo o use strict
+  - Jak wlasciwie dziala this na poczatku funkcji.
   */
 
-const titleClickHandler = function(event) {
-  event.preventDefault();
-  const clickedElement = this;
+  const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
 
-  /* [DONE] remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
-  const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+
+    /* [DONE] add class 'active' to the clicked link */
+
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
+
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
   }
 
-  /* [DONE] add class 'active' to the clicked link */
+  const links = document.querySelectorAll('.titles a');
 
-  clickedElement.classList.add('active');
-
-  /* [DONE] remove class 'active' from all articles */
-
-  const activeArticles = document.querySelectorAll('.posts .active');
-
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
   }
 
-  /* [DONE] get 'href' attribute from the clicked link */
 
-  const articleSelector = clickedElement.getAttribute('href');
+    /* NEXT STEP */
 
-  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-  const targetArticle = document.querySelector(articleSelector);
+  function generateTitleLinks(){
 
-  /* [DONE] add class 'active' to the correct article */
+    /* [DONE] remove contents of titleList */
 
-  targetArticle.classList.add('active');
-}
+    const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
 
-const links = document.querySelectorAll('.titles a');
+    /* for each article */
+    // mozliwa zamiana argumentu .post na nazwe stalej optArticleSelector
+    const articles = document.querySelectorAll('.post');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+    for(let article of articles){
+
+    }
+
+      /* get the article id */
+
+      const articleId = element.getAttribute('article');
+
+      /* find the title element */
+
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* get the title from the title element */
+
+      /* create HTML of the link */
+
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log(linkHTML);
+      /* insert link into titleList */
+  }
+
+generateTitleLinks();
+
 }
